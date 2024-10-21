21d5778789d26015a18c83d4ce64561c6e79745e
furkanislek <furkanakifislek@gmail.com>
2024-10-21 22:24:51 +0300

    hedef ekleme ekranı eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:students_follow_app/pages/auth/login_register_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('tr_TR', null);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
